02a37173d93daaa23945351c501cdd9e961a30f1
Shallowbeing <load_more@163.com>
2021-12-17 15:56:21 +0800

    Init Project.

---
+++ README.md
@@ -0,0 +1,3 @@
+## 流程图
+
+![](https://gitee.com/gainmore/imglib/raw/master/img/20211217153819.png)
---
+++ app/main/index.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+let mainWindow = null
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+
+  win.loadFile(path.join(__dirname, '../renderer/index.html'))
+
+  return win
+}
+
+app.on('ready', () => {
+  mainWindow = createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
